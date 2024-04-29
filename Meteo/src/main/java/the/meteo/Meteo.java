5c3de229b7744dba66874173d1e163515479cf68
mxhalyfalvx <159784349+mxhalyfalvx@users.noreply.github.com>
2024-04-29 19:36:22 +0200

    Enter fixed

--- Meteo/src/main/java/the/meteo/Meteo.java
+++ Meteo/src/main/java/the/meteo/Meteo.java
@@ -5,6 +5,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -73,6 +74,22 @@ public class Meteo extends JFrame {
         locationPanel.add(locationLabel);
 
         setVisible(true);
+        
+        // Add KeyListener to the frame to listen for Esc key press
+        addKeyListener(new KeyListener() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+                    System.exit(0);
+                }
+            }
+
+            @Override
+            public void keyTyped(KeyEvent e) {}
+
+            @Override
+            public void keyReleased(KeyEvent e) {}
+        });
     }
 
     private String[] createHourOptions() {
